e7bd19b3208201762fe1c1ef642d683219416aa2
username <htqxyz@gmail.com>
2022-12-01 01:36:43 +0900

    Initial commit

---
+++ javascript_library/vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import {resolve} from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/comp/index.ts'),
+      name: 'front-lib-skeleton',
+      fileName: format => `index.${format}.js`
+    },
+    rollupOptions: {
+      external: ['react'],
+      output: {
+        globals: {
+          react: 'React'
+        }
+      }
+    }
+  }
+})
